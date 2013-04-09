d1cd30903b55c0e1acce8b5e6e318b0766c0f697
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-04-09 19:33:15 +0300

    Events list.

--- src/VilniusPHP/EventsBundle/Controller/DefaultController.php
+++ src/VilniusPHP/EventsBundle/Controller/DefaultController.php
@@ -6,14 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/events")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * Events list.
+     *
+     * @Route("/", name="events.list")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        $repository = $this->getDoctrine()->getRepository('VilniusPHPEventsBundle:Event');
+
+        $events = $repository->findBy(array(), array('date' => 'DESC'));
+
+        return array('events' => $events);
     }
 }
