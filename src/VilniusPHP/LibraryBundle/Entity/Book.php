c4cb24f750e26ce528ecd83618878b5b3edbd469
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-04-18 18:57:47 +0300

    Book entity

---
+++ src/VilniusPHP/LibraryBundle/Entity/Book.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace VilniusPHP\LibraryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use VilniusPHP\UserBundle\Entity\User;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity
+ */
+class Book
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subtitle", type="string", length=255, nullable=true)
+     */
+    private $subtitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="edition", type="string", length=50, nullable=true)
+     */
+    private $edition;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255, nullable=true)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="homepage", type="string", length=255, nullable=true)
+     */
+    private $homepage;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cover", type="string", length=255, nullable=true)
+     */
+    private $cover;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="\VilniusPHP\UserBundle\Entity\User")
+     * @ORM\JoinTable(name="book_owner",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="book_id", referencedColumnName="id")}
+     * )
+     */
+    private $owners;
+
+    public function __construct()
+    {
+        $this->owners = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set subtitle
+     *
+     * @param string $subtitle
+     * @return Book
+     */
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+    
+        return $this;
+    }
+
+    /**
+     * Get subtitle
+     *
+     * @return string 
+     */
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * Set edition
+     *
+     * @param string $edition
+     * @return Book
+     */
+    public function setEdition($edition)
+    {
+        $this->edition = $edition;
+    
+        return $this;
+    }
+
+    /**
+     * Get edition
+     *
+     * @return string 
+     */
+    public function getEdition()
+    {
+        return $this->edition;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Book
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set homepage
+     *
+     * @param string $homepage
+     * @return Book
+     */
+    public function setHomepage($homepage)
+    {
+        $this->homepage = $homepage;
+    
+        return $this;
+    }
+
+    /**
+     * Get homepage
+     *
+     * @return string 
+     */
+    public function getHomepage()
+    {
+        return $this->homepage;
+    }
+
+    /**
+     * Set cover
+     *
+     * @param string $cover
+     * @return Book
+     */
+    public function setCover($cover)
+    {
+        $this->cover = $cover;
+    
+        return $this;
+    }
+
+    /**
+     * Get cover
+     *
+     * @return string 
+     */
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Get owners
+     * 
+     * @return ArrayCollection
+     */
+    public function getOwners()
+    {
+        return $this->owners;
+    }
+
+    /**
+     * Add owner
+     * 
+     * @param User $owner
+     *
+     * @return Book
+     */
+    public function addOwner(User $owner)
+    {
+        $this->owners[] = $owners;
+
+        return $this;
+    }
+}
