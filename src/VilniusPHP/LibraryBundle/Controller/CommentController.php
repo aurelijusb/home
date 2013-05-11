0b45f13f5623a458e86988f7af22ba2bbcc2d6bb
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-05-11 15:36:38 +0300

    Added reviews section for books

--- src/VilniusPHP/LibraryBundle/Controller/CommentController.php
+++ src/VilniusPHP/LibraryBundle/Controller/CommentController.php
@@ -26,7 +26,31 @@ class CommentController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('VilniusPHPLibraryBundle:Comment')
-            ->findBy(array('book' => $bookId), array('date' => 'DESC'));
+            ->findBy(
+                array('book' => $bookId, 'review' => 0),
+                array('date' => 'DESC')
+            );
+
+        return array(
+            'entities' => $entities
+        );
+    }
+
+    /**
+     * List all reviews of Book.
+     * 
+     * @Method("GET")
+     * @Template()
+     */
+    public function reviewsAction($bookId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('VilniusPHPLibraryBundle:Comment')
+            ->findBy(
+                array('book' => $bookId, 'review' => 1),
+                array('date' => 'DESC')
+            );
 
         return array(
             'entities' => $entities
