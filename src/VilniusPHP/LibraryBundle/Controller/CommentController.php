2d0e82c70f2eb33c8926bbab0fd936cbd76f66e7
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-05-11 15:25:31 +0300

    Display comments list on book page.

---
+++ src/VilniusPHP/LibraryBundle/Controller/CommentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace VilniusPHP\LibraryBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use VilniusPHP\LibraryBundle\Entity\Book;
+
+/**
+ * Comment controller.
+ */
+class CommentController extends Controller
+{
+
+    /**
+     * List all comments of Book.
+     * 
+     * @Method("GET")
+     * @Template()
+     */
+    public function listAction($bookId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('VilniusPHPLibraryBundle:Comment')
+            ->findBy(array('book' => $bookId), array('date' => 'DESC'));
+
+        return array(
+            'entities' => $entities
+        );
+    }
+}
